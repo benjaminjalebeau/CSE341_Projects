e9d53ba8ec52f03f0658915b7f65d6608491c27b
benjaminjalebeau <benjamin.ja.lebeau@gmail.com>
2024-11-07 20:42:50 -0700

    actually fixed getsingle

--- Contacts/controllers/users.js
+++ Contacts/controllers/users.js
@@ -2,11 +2,11 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getSingle = async (req, res) => {
-    const userId = new Object(req.params.id);
-    const result = await mongodb.getDatabase().db('project1').collection('users').find({_id: userId});
+    const userId = ObjectId.createFromHexString(req.params.id);
+    const result = await mongodb.getDatabase().db('project1').collection('users').find({ _id: userId });
     result.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users);
+        res.status(200).json(users[0]);
     });
 
 };
