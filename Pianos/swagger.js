a077549a32a9e9915d4964c4c4324f1ab2263588
benjaminjalebeau <benjamin.ja.lebeau@gmail.com>
2024-11-25 16:20:16 -0700

    removed an extra https

--- Pianos/swagger.js
+++ Pianos/swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'Pianos Api',
         description: 'Pianos Api'
     },
-    host: 'https://cse341-pianos.onrender.com',
+    host: 'cse341-pianos.onrender.com',
     schemes: ['http', 'https']
 };
 
